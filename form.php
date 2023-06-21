1729c5009bbcee4b0beb7365c8297115348189b5
Sreedeva <79521521+sreedevareddy@users.noreply.github.com>
2023-06-21 13:24:48 +0530

    first commit

---
+++ form.php
@@ -0,0 +1,59 @@
+<?php 
+// Email configuration 
+$toEmail = 'shubhavratkulkarni@gmail.com'; 
+$fromName = 'Sender Name'; 
+$formEmail = 'sender@example.com'; 
+ 
+$postData = $statusMsg = $valErr = ''; 
+$status = 'error'; 
+ 
+// If the form is submitted 
+if(isset($_POST['submit'])){ 
+    // Get the submitted form data 
+    $postData = $_POST; 
+    $name = trim($_POST['first-name']); 
+    $lastname = trim($_POST['last-name']); 
+    $email = trim($_POST['email']); 
+    $message = trim($_POST['message']); 
+     
+    // Validate form fields 
+    if(empty($name)){ 
+         $valErr .= 'Please enter your name.<br/>'; 
+    } 
+    if(empty($lastname) || filter_var($lastname, FILTER_VALIDATE_EMAIL) === false){ 
+        $valErr .= 'Please enter a valid last name.<br/>'; 
+    } 
+    if(empty($email)){ 
+        $valErr .= 'Please enter valid email.<br/>'; 
+    } 
+    if(empty($message)){ 
+        $valErr .= 'Please enter your message.<br/>'; 
+    } 
+     
+    if(empty($valErr)){ 
+        // Send email notification to the site admin 
+        $subject = 'New contact request submitted'; 
+        $htmlContent = " 
+            <h2>Contact Request Details</h2> 
+            <p><b>Name: </b>".$name."</p> 
+            <p><b>Email: </b>".$lastname."</p> 
+            <p><b>Subject: </b>".$subject."</p> 
+            <p><b>Message: </b>".$message."</p> 
+        "; 
+         
+        // Always set content-type when sending HTML email 
+        $headers = "MIME-Version: 1.0" . "\r\n"; 
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+        // Header for sender info 
+        $headers .= 'From:'.$fromName.' <'.$formEmail.'>' . "\r\n"; 
+         
+        // Send email 
+        @mail($toEmail, $subject, $htmlContent, $headers); 
+         
+        $status = 'success'; 
+        $statusMsg = 'Thank you! Your contact request has submitted successfully, we will get back to you soon.'; 
+        $postData = ''; 
+    }else{ 
+        $statusMsg = '<p>Please fill all the mandatory fields:</p>'.trim($valErr, '<br/>'); 
+    } 
+}
